98e916a50fb6969a594951671d206c30fe902b1e
akshayah3 <akshaynukala95@gmail.com>
2014-07-13 17:09:25 +0530

    First problem

---
+++ spoj1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 13 16:55:12 2014
+
+@author: akshay
+"""
+
+a = 0
+while(a == 0):
+    b = int(raw_input())
+    if b < 100:
+        if b == 42:
+            break
+        else:
+            print b
